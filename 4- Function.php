b2be148f69bb43f693c8a6d0cf1840cef6e3da8e
mohammad hosseinzadeh <mohammadhz98@yahoo.com>
2021-07-07 18:42:18 +0430

    some more learning 1400-04-16

--- 4- Function.php
+++ 4- Function.php
@@ -1,9 +1,20 @@
 <?php
 
-    funcName();
-
-    function funcName(){
+    function funcSimple(){
         echo "hello world!";
     }
+    funcSimple();
+
+
+    function funcWithInput($input){
+        echo "Input = ".$input;
+    }
+    funcWithInput("some text");
+
+    function funcWithReturn($input1, $input2){
+        return $input1 + $input2;
+    }
+    $resault = funcWithReturn(5, 10);
+    echo $resault;
 
 ?>
